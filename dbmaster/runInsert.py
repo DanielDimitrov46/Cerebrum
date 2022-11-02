861b1ee5f0e86501b727a02d79d9f0dae42b526e
Daniel Dimitrov <dani.dimitrov.snack9@gmail.com>
2022-11-02 13:09:23 +0200

    almost connect search function
    its connected, but need more logic about parameters to search, and need beauty printing

--- dbmaster/runInsert.py
+++ dbmaster/runInsert.py
@@ -2,14 +2,14 @@ import dbmaster, os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 params = {
-    'name': 'Pesho',
-    'family': 'Danchov',
-    'num': '20601',
-    'town': 'Pravets',
-    'G' : 'M',
-    'rUm' : '401',
-    'birthDate' : '2006-01-24',
-    'scor' : '5.31'
+    'name': input(),
+    'family': input(),
+    'num': input(),
+    'town': input(),
+    'G' : input(),
+    'rUm' : input(),
+    'birthDate' : input(),
+    'scor' : input()
 }
 
 f = dbmaster.open('db.txt')
