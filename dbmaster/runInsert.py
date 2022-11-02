087a1375e39649cb9389c682f01a777126b58aec
Daniel Dimitrov <dani.dimitrov.snack9@gmail.com>
2022-11-02 16:06:03 +0200

    sdfds

--- dbmaster/runInsert.py
+++ dbmaster/runInsert.py
@@ -1,16 +1,17 @@
 import dbmaster, os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+name = input("Enter a database name:")
 params = {
-    'name': input(),
-    'family': input(),
-    'num': input(),
-    'town': input(),
-    'G' : input(),
-    'rUm' : input(),
-    'birthDate' : input(),
-    'scor' : input()
+    'name': input("Enter a name:"),
+    'family': input("Enter a family:"),
+    'num': input("Enter a course number:"),
+    'town': input("Enter a town:"),
+    'G' : input("Enter a gender"),
+    'rUm' : input("Enter a room:"),
+    'birthDate' : input("Enter a birth date:"),
+    'scor' : input("Enter average scor")
 }
 
-f = dbmaster.open('db.txt')
+f = dbmaster.open(f'{name}.txt')
 f.insert(params)
